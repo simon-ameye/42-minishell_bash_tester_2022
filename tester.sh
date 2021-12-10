d780fc0b13c36897ca1463c9a109400ea9a86f76
Simon AMEYE <sameye@e2r5p2.clusters.42paris.fr>
2021-12-10 16:31:41 +0100

    add new

--- tester.sh
+++ tester.sh
@@ -9,6 +9,7 @@ NOCOLOR='\033[0m'
 make minishell -C $minishell_dir > /dev/null
 cp $minishell_dir/minishell .
 rm -rf trace
+mkdir trace
 
 function test()
 {
@@ -27,16 +28,15 @@ function test()
 	#-------------cmp-------------
 	diff tmp/ref tmp/res > tmp/diff
 	if [ -s tmp/diff ]; then 
-		printf "$GRE[ OK ]\n$NOCOLOR"
-	else
 		printf "$RED[ KO ]\n$NOCOLOR"
 		echo "you failed at" $@
-		echo "please check your trace in /trace" $@
-		mkdir trace
+		echo "please check your trace in /trace/" $@
 		echo "diff :"
 		cat tmp/diff > trace/$@_diff
 		cat tmp/res > trace/$@_your_output
 		cat tmp/res > trace/$@_ref_output
+	else
+		printf "$GRE[ OK ]\n$NOCOLOR"
 	fi
 	#-------------del-------------
 	rm -rf tmp/
